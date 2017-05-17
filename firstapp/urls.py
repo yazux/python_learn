35d59b0acd88c16b09974bd66c3a0bb1f2e19812
yazux <speed.live@mail.ru>
2017-05-17 13:52:30 +1000

    Added views to create/edit blog post inside the site.

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post, name='post'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
 
 
 def index(request):
@@ -11,3 +13,31 @@ def index(request):
 def post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'app/post.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'app/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'app/post_edit.html', {'form': form})
