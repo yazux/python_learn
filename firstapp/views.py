820668a4e13860daf887b8d83098c6a200b98c06
yazux <speed.live@mail.ru>
2017-05-17 13:15:36 +1000

    add detail post page

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
+
 def index(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/index.html', {'posts': posts})
+    return render(request, 'app/posts.html', {'posts': posts})
+
+
+def post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'app/post.html', {'post': post})
